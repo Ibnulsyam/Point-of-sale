d3e40de64ae2c2a95bba2d70bffb367d8db950a9
ibnul syam <ibnulsyam6@gmail.com>
2022-06-23 11:51:42 +0800

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
     );
 
     swal({
-      title: "Success!",
+      title: "Success",
       text: `${dataProduct.name} has been Added!`,
       icon: "success",
       button: "OK",
